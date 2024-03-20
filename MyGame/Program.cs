c521d3054b54347b106171cad20895e0d9f875a0
notCroptu <martinsmariana1972@gmail.com>
2024-03-20 20:58:16 +0000

    Ex6 - MyGame

---
+++ MyGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyGame
+{
+    public class Enemy
+    {
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
