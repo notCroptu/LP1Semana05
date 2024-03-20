8aba0350ec13d087ddf9011d5a9f9f20a3c3f38d
notCroptu <martinsmariana1972@gmail.com>
2024-03-20 22:03:52 +0000

    Ex9 - MyGame

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace MyGame
 {
@@ -9,16 +10,45 @@ namespace MyGame
         private float shield;
         public Enemy(string name)
         {
-            this.name = name; // name o c u l t a t h i s . name
+            this.name = SetName(name); // name o c u l t a t h i s . name
             health = 100;
             shield = 0;
+        }
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damagestilltoinflict = -shield;
+                shield = 0;
+                health -= damagestilltoinflict;
+                if (health < 0) health = 0;
+            }
+        }
+        public string SetName(string name)
+        {
+            StringBuilder newname = new StringBuilder(name, 0, 8, 50);
+            return newname.ToString();
+        }
+        public float GetHealth()
+        {
+            return health;
+        }
+        public float GetShield()
+        {
+            return shield;
+        }
+        public string GetName()
+        {
+            return name;
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Enemy slime = new Enemy("slime6789-10-11");
+            Console.WriteLine(slime.GetName());
         }
     }
 }
