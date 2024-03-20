95b0cf44293996d3677b5cfe66160d45afe6d87e
notCroptu <martinsmariana1972@gmail.com>
2024-03-20 20:04:12 +0000

    Ex5 - RanDice

---
+++ RanDice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RanDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(args[0]);
+            int s = int.Parse(args[1]);
+
+            int result = 0;
+
+            Random random = new Random(s);
+
+            for ( int i = 0; i < n; i++)
+            {
+                result += random.Next(1, 7);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
