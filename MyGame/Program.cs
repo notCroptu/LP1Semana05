91691aef68a805100510bc4518d79d37df58edd0
notCroptu <martinsmariana1972@gmail.com>
2024-03-20 21:18:30 +0000

    Ex7 - MyGame

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -4,7 +4,15 @@ namespace MyGame
 {
     public class Enemy
     {
-        
+        private string name;
+        private float health;
+        private float shield;
+        public Enemy(string name)
+        {
+            this.name = name; // name o c u l t a t h i s . name
+            health = 100;
+            shield = 0;
+        }
     }
     class Program
     {
