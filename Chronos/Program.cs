f6fd40e9d9e3cc3307694db2fbba45636b878471
notCroptu <martinsmariana1972@gmail.com>
2024-03-20 12:19:26 +0000

    Ex2 - Chronos

---
+++ Chronos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+            crono1.Start();
+            Thread.Sleep(600);
+            crono2.Start();
+            Thread.Sleep(200);
+            crono1.Stop();
+            crono2.Stop();
+
+            TimeSpan TimeCrono1 = crono1.Elapsed;
+            TimeSpan TimeCrono2 = crono2.Elapsed;            
+
+            Console.WriteLine($"{TimeCrono1.Milliseconds/1000f:f3}");
+            Console.WriteLine($"{TimeCrono2.Milliseconds/1000f:f3}");
+        }
+    }
+}
